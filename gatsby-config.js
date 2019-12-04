4051a74a9123acda4d630ebba3335eb246bba6b0
Takatoshi Wada <fueledbytaching@gmail.com>
2019-12-04 22:56:13 +0900

    try

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,8 +50,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: "95jn26viorf7",
+        accessToken: "xHiGj2ggfRc2aZBXbHJAMyKyg2lB1exURGLdkmPnpu8",
       },
     },
     {
